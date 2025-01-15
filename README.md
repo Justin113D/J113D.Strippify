d883a1abb9612c3595aba82fafaa02772bcf5f35
Justin113D <Justin113D@gmail.com>
2025-01-15 16:37:24 +0100

    Initial setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# J113D.Strippify
+
+A triangle strippification algorithm.
+
+Based on the paper written by David Kronmann (with modifications):
+https://pdfs.semanticscholar.org/9749/331d92f865282c3f5a19b73b25c4f0ac02bc.pdf
---
+++ src/J113D.Strippify/TopologyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace J113D.Strippify
+{
+	/// <summary>
+	/// Gets raised when certain 3D topology rules are ignored and/or not processable
+	/// </summary>
+	public class TopologyException : Exception
+	{
+		/// <summary>
+		/// Creates a new topology exception.
+		/// </summary>
+		/// <param name="message">The message to pass along.</param>
+		public TopologyException(string? message) : base(message) { }
+	}
+}
